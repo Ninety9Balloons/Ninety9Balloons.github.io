c7ab0abedfbdf0c1fb4d028345ad938c3e0b54f7
Troy <tdreuther@gmail.com>
2026-06-04 21:37:16 -0400

    fixed file names

--- src/pages/MotionGFX.jsx
+++ src/pages/MotionGFX.jsx
@@ -5,37 +5,37 @@ const projects = [
     title: "Polandball 17",
     tags: ["Animation", "YouTube"],
     vimeoUrl: "https://vimeo.com/819309924?fl=pl&fe=cm",
-    image: "/images/polans flag tn.png",
+    image: "/images/polans-flag-tn.png",
   },
   {
     title: "History of Europe",
     tags: ["Animation", "YouTube"],
     vimeoUrl: "https://vimeo.com/815361161?fl=pl&fe=cm",
-    image: "/images/history tn.png",
+    image: "/images/history-tn.png",
   },
   {
     title: "End of the End",
     tags: ["Animation", "YouTube"],
     vimeoUrl: "https://vimeo.com/815361127?fl=pl&fe=cm",
-    image: "/images/end of the end tn.png",
+    image: "/images/end-of-the-end-tn.png",
   },
   {
     title: "The Legend of Stev Ep. 1",
     tags: ["Animation", "Comedy"],
     vimeoUrl: "https://vimeo.com/815361752?fl=pl&fe=cm",
-    image: "/images/stev ep1 tn.png",
+    image: "/images/stev-ep1-tn.png",
   },
   {
     title: "The Legend of Stev Ep. 3",
     tags: ["Animation", "Comedy"],
     vimeoUrl: "https://vimeo.com/815361574?fl=pl&fe=cm",
-    image: "/images/stev tn.png",
+    image: "/images/stev-tn.png",
   },
   {
     title: "Saturday Down South",
     tags: ["Motion Graphics", "Sports"],
     vimeoUrl: "https://vimeo.com/815361692?fl=pl&fe=cm",
-    image: "/images/sds tn.png",
+    image: "/images/sds-tn.png",
   },
 ];
 
